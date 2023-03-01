feab4b9d43d4a269c2a620af57fe5c6b36fe5424
Nann Ei Ei Win <nanneieiwinn37@gmail.com>
2023-03-01 12:52:25 +0630

    Remove default text

--- scripts/app.js
+++ scripts/app.js
@@ -1,17 +1,18 @@
-let btn = document.querySelector('button')
-let adviceDisplay = document.querySelector('h1')
-let adviceID = document.getElementById('ID')
+let btn = document.querySelector('button');
+let adviceDisplay = document.querySelector('h1');
+let adviceID = document.getElementById('ID');
 
 btn.addEventListener('click', getAdvice)
+
 function getAdvice(event) {
-  event.preventDefault()
-  let apiUrl = 'https://api.adviceslip.com/advice'
+  event.preventDefault();
+  let apiUrl = 'https://api.adviceslip.com/advice';
   axios.get(apiUrl).then(response => {
-    let apiId = response.data.slip.id
-    let advice = response.data.slip.advice
-    adviceID.innerHTML = apiId
+    let apiId = response.data.slip.id;
+    let advice = response.data.slip.advice;
+    adviceID.innerHTML = apiId;
     adviceDisplay.innerHTML = `"${advice}"`
   })
 }
 
-btn.click()
+btn.click();
